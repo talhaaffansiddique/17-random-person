eb962183878c811fc86f5cf13b1a53e5ad66dccc
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-11-13 11:08:14 +0500

    208 - Random User Fetch User

--- src/App.js
+++ src/App.js
@@ -12,8 +12,44 @@ const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 function App() {
   const [loading, setLoading] = useState(true);
   const [person, setPerson] = useState(null);
+  const [value, setValue] = useState("Random Person");
   const [title, setTitle] = useState("name");
-  const [value, setValue] = useState("random person");
+
+  const getPerson = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const data = await response.json();
+    const person = data.results[0];
+    const { phone, email } = person;
+    const { large: image } = person.picture;
+    const {
+      login: { password },
+    } = person;
+    const { first, last } = person.name;
+    const {
+      dob: { age },
+    } = person;
+    const {
+      street: { number, name },
+    } = person.location;
+    const newPerson = {
+      image,
+      phone,
+      email,
+      password,
+      age,
+      street: `${number} ${name}`,
+      name: `${first} ${last}`,
+    };
+    setPerson(newPerson);
+    setLoading(false);
+    setTitle("name");
+    setValue(newPerson.name);
+  };
+
+  useEffect(() => {
+    getPerson();
+  }, []);
 
   const handleValue = (e) => {
     console.log(e.target);
@@ -76,7 +112,7 @@ function App() {
               </button>
             </div>
 
-            <button className="btn" type="button">
+            <button className="btn" type="button" onClick={getPerson}>
               {loading ? "loading..." : "random user"}
             </button>
           </div>
